7b827037cd9a5db3d3aba5307408487f0e3eaa2f
SalikPaudel <36765526+Sallikrampaudel@users.noreply.github.com>
2024-09-09 22:23:54 -0500

    Update wordcount.py

--- python/wordcount.py
+++ python/wordcount.py
@@ -1,1 +1,33 @@
 
+def list_to_dict(list):
+    counter = {}
+    for word in list:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+    return counter
+
+def frequecy(dict):
+    maxkey = dict["hello"]
+    i=0
+    for x in dict:
+        if (dict[x] > i):
+            i = dict[x]
+            maxkey = x
+    new_list = []
+    for y in dict:
+        if dict[maxkey]==dict[y]:
+            dict[maxkey] = dict[y]
+            new_list.append(y)
+
+    return new_list
+
+if __name__ == '__main__' :
+
+    input_paragraph = "Hello world Hello world This world is full of surprises Surprises are everywhere; surprises are fun"
+    words=input_paragraph.lower().split()
+    dist = list_to_dict(words)
+    max=frequecy(dist)
+    print(dist)
+    print("Most frequent word(s):", max)
