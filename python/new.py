5218c66faffb3bd801562248e1055c64c79a50ea
Salik <paudelsalik4@gmail.com>
2025-01-06 18:59:53 -0600

    Plaindrom

---
+++ python/new.py
@@ -0,0 +1,14 @@
+
+def longestCommonPrefix(strs: list[str]) -> str:
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+
+strs = ["flow","flower","flight"]
+print(longestCommonPrefix(strs))
